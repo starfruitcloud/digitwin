7a8b16e353d5b47be6f37b0a6489a9ce6db95194
dezhizhang <1541609448@qq.com>
2024-08-29 14:45:54 +0800

    feat: 支持ts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import checker from "vite-plugin-checker";
 const cesiumSource = "node_modules/cesium/Build/Cesium";
 const cesiumBaseUrl = "cesiumStatic";
 
@@ -8,6 +9,10 @@ export default defineConfig({
     CESIUM_BASE_URL: JSON.stringify(`/${cesiumBaseUrl}`),
   },
   plugins: [
+    // 支持ts
+    checker({
+      typescript: true,
+    }),
     // 将cesium的静态文件复制到工作区
     viteStaticCopy({
       targets: [
