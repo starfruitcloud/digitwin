e237c7fcad7f94a3c276fa33f59948e1db45c04a
dezhizhang <1541609448@qq.com>
2024-08-31 10:33:23 +0800

    feat: 抛物流动飞线

--- src/lib/line/VerticalFlyLineLight.ts
+++ src/lib/line/VerticalFlyLineLight.ts
@@ -1,11 +1,11 @@
 /*
- * :file description: 
+ * :file description: 随机竖直飞线
  * :name: /firethorn/src/lib/line/VerticalFlyLineLight.ts
  * :author:张德志
  * :copyright: (c) 2024, Xiaozhi
  * :date created: 2024-08-31 08:53:22
  * :last editor: 张德志
- * :date last edited: 2024-08-31 09:17:25
+ * :date last edited: 2024-08-31 09:21:37
  */
 import * as Cesium from 'cesium';
 import LineFlyMaterialProperty from '@/material/LineFlyMaterialProperty';
